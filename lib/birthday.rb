e98eadd049c04be1fa58a8c70d22518d4ce581f4
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-05 11:21:53 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,5 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each {|kids_name, age| puts "Happy Birthday #{kids_name}! You are now #{age} years old!"}
+  birthday_kids.each { |kids_name, age|
+    if age < 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  }
 end
